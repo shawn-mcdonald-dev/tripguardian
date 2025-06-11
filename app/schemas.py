9398d5868d4e2d03eac39847d7a67b7de6dd7d48
Shawn McDonald <shawnmcdon01@gmail.com>
2025-06-11 12:31:21 +0200

    added classes to contain flight info (schemas.py), new post in main.py

---
+++ app/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import List, Dict
+
+class FlightLeg(BaseModel):
+    from_: str
+    to: str
+    delay_mins: int
+
+class DisruptionRequest(BaseModel):
+    trip_id: str
+    flights: List[FlightLeg]
+    weather: Dict[str, str]
+    current_location: str
